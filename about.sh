91041585c2326d6ad7b2cc6f53ac85ab5ed28bdc
jhoy-id <89542179+jhoy-id@users.noreply.github.com>
2021-08-26 04:11:33 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,14 +2,14 @@
 
 clear
 echo -e "=================================================" | lolcat
-echo -e "#        Premium Auto Script By E-Jhoy       #" | lolcat
+echo -e "#        Premium Auto Script By E-Jhoy          #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
 echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
 echo -e "# Build Up By : _________                       #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# Thanks To : For anu tos ngabantu              #" | lolcat
+echo -e "# Thanks To : Kamu                              #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# Commant :                                     #" | lolcat
 echo -e "#         ┌┬┐┌─┐┌┐┌┌─┐┬ ┬   ┬┌─┐┬─┐┌─┐          #" | lolcat
