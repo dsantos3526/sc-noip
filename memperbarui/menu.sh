ab1602e0547a5348e501b88060735965b03c3cab
jhoy-id <89542179+jhoy-id@users.noreply.github.com>
2021-08-26 04:45:18 +0700

    Update menu.sh

--- memperbarui/menu.sh
+++ memperbarui/menu.sh
@@ -28,7 +28,7 @@ IPVPS=$(curl -s ipinfo.io/ip )
     echo -e    "    \e[032;1mTime:\e[0m $WKT"
     echo -e    "    \e[033;1mIPVPS:\e[0m $IPVPS"
 echo -e  ""
-echo -e  "  ========================MENU OPTIONS=======================" | lolcat
+echo -e  "  ==========================MENU OPTIONS=========================" | lolcat
 echo -e   "   1\e[1;33m)\e[m SSH & OpenVPN Menu"
 echo -e   "   2\e[1;33m)\e[m Panel Wireguard "
 echo -e   "   3\e[1;33m)\e[m Panel L2TP & PPTP Account"
@@ -37,9 +37,9 @@ echo -e   "   5\e[1;33m)\e[m Panel SSR & SS Account"
 echo -e   "   6\e[1;33m)\e[m Panel V2Ray"
 echo -e   "   7\e[1;33m)\e[m Panel VLess"
 echo -e   "   8\e[1;33m)\e[m Panel TRojan"
-echo -e   "  \e[1;32m===========================================================\e[m" | lolcat
-echo -e   "                         SYSTEM MENU\e[m" | lolcat 
-echo -e   "  \e[1;32m===========================================================\e[m" | lolcat
+echo -e   "  \e[1;32m===============================================================\e[m" | lolcat
+echo -e   "                           SYSTEM MENU\e[m" | lolcat 
+echo -e   "  \e[1;32m===============================================================\e[m" | lolcat
 echo -e   "   9\e[1;33m)\e[m  Add Subdomain Host For VPS"
 echo -e   "   10\e[1;33m)\e[m Renew Certificate V2RAY"
 echo -e   "   11\e[1;33m)\e[m Change Port All Account"
@@ -55,9 +55,9 @@ echo -e   "   20\e[1;33m)\e[m Information Display System"
 echo -e   "   21\e[1;33m)\e[m Info Script Auto Install"
 echo -e   "   22\e[1;33m)\e[m Install BBR"
 echo -e   "   23\e[1;33m)\e[m Clear-Log"
-echo -e   "  \e[1;32m===========================================================\e[m" | lolcat
+echo -e   "  \e[1;32m===============================================================\e[m" | lolcat
 echo -e   "   x)   Exit" | lolcat
-echo -e   "  \e[1;32m===========================================================\e[m" | lolcat
+echo -e   "  \e[1;32m===============================================================\e[m" | lolcat
 echo -e   ""
 read -p "     Select From Options [1-8 or x] :  " menu
 echo -e   ""
