e077153d5a00a87910e3c8e0417e1f838c345106
jhoy-id <89542179+jhoy-id@users.noreply.github.com>
2021-11-10 15:51:56 +0700

    Add files via upload

---
+++ ohp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/jhoy-id/sc-noip/main/ohp-db.sh;chmod +x ohp-db.sh;./ohp-db.sh
+wget https://raw.githubusercontent.com/jhoy-id/sc-noip/main/ohp-ovpn.sh;chmod +x ohp-ovpn.sh;./ohp-ovpn.sh
